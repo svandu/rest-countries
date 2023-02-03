424ddde7a5ec5cffbb8eb99470b80cc855d720d2
svandu <svandu08@gmail.com>
2023-02-03 23:56:19 +0530

    routes, route add

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+
+//components
 import ContainerNav from '../components/ContainerNav'
-import HeroSection from '../components/HeroSection'
+import MainContainer from '../components/MainContainer'
+import IndividualCard from '../components/IndividualCard'
+
+//styles
 import '../assets/page-style/App.css'
+
+//routing
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="root-container">
-      <div className="hero-sec">
-        <HeroSection />
-        <ContainerNav />  
+      <ContainerNav />
+      <div className='main-container'>
+        <Routes>
+              <Route path='/' element={<MainContainer />}></Route>
+              <Route path='/IndividualCard' element={<IndividualCard />}></Route>
+          </Routes>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
