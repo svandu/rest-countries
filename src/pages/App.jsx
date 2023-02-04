847981e51f53259037075a6f63435b904e6e941f
svandu <svandu08@gmail.com>
2023-02-05 00:55:35 +0530

    add theme in app

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 //components
 import ContainerNav from '../components/ContainerNav'
@@ -11,18 +11,34 @@ import '../assets/page-style/App.css'
 //routing
 import { Routes, Route } from "react-router-dom";
 
+//theme
+import {createContext} from "react";
+
+export const ThemeContext = createContext(null);
+
 function App() {
+
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr == "light" ? "dark" : "light"));
+  }
+
   return (
-    <div className="root-container">
-      <ContainerNav />
-      <div className='main-container'>
-        <Routes>
-              <Route path='/' element={<MainContainer />}></Route>
-              <Route path='/IndividualCard' element={<IndividualCard />}></Route>
-              <Route path='/MainContainer' element={<MainContainer/>}></Route>
-          </Routes>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <div className="root-container" id={theme}>
+          <div className="container-navbar">
+            <ContainerNav />
+          </div>
+          <div className='main-container'>
+            <Routes>
+                <Route path='/' element={<MainContainer />}></Route>
+                <Route path='/IndividualCard' element={<IndividualCard />}></Route>
+                <Route path='/MainContainer' element={<MainContainer/>}></Route>
+            </Routes>
+        </div>
       </div>
-    </div>
+    </ThemeContext.Provider>
   )
 }
 
